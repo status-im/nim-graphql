7b4bc109d7625f8b4bc7ddb7c4ade667897e23d6
jangko <jangko128@gmail.com>
2021-05-07 11:15:30 +0700

    add new response stream: NodeRespStream
    it will return the AST produced by execution engine
    or a clone of that AST

---
+++ graphql/builtin/node_respstream.nim
@@ -0,0 +1,37 @@
+# nim-graphql
+# Copyright (c) 2021 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
+import
+  ../common/[respstream, ast]
+
+export respstream
+
+type
+  NodeRespStream* = ref object of RootRef
+    cloneTree: bool
+    node: Node
+
+proc serialize*(resp: NodeRespStream, n: Node) =
+  if resp.cloneTree:
+    resp.node = copyTree(n)
+    return
+
+  # simply return the same tree
+  resp.node = n
+
+proc getNode*(resp: NodeRespStream): Node =
+  resp.node
+
+proc init*(v: NodeRespStream, cloneTree: bool = false) =
+  v.cloneTree = cloneTree
+
+proc new*(_: type NodeRespStream, cloneTree: bool = false): NodeRespStream =
+  let v = NodeRespStream()
+  v.init(cloneTree)
+  v
